3101015e3d04f7b2928acd0f3e0bc5ddacf96745
Brayden Phillips <bphillips@playvox.com>
2022-06-28 19:47:12 +1000

    wip

--- apps/api-billing-commands/src/create-payment-intent/handler.ts
+++ apps/api-billing-commands/src/create-payment-intent/handler.ts
@@ -1,13 +1,15 @@
-import {APIGatewayEvent} from 'aws-lambda';
-import {formatError, formatResponse} from "@evento/api-utils";
+import { APIGatewayEvent } from 'aws-lambda';
+import { formatError, formatResponse } from '@evento/api-utils';
 import Stripe from 'stripe';
-import {EventoApi} from "@evento/api-client";
-import {isLeft} from 'fp-ts/Either';
-import {config} from '../../config';
+import { PageModel } from '@evento/models';
+import { TestConfig } from './test-config';
 
-const stripe = new Stripe('sk_test_wsFx86XDJWwmE4dMskBgJYrt', {
-    apiVersion: "2020-08-27"
-});
+const stripe = new Stripe(
+    'sk_test_51H5lSlIHLTnuvRM7IUHjny0aZtV6WXsqrI9X4PkdfeDwMhN6o3Nc9TaZpOvtwFHZYM9Of4wPMSoNq8hQH7VBFzXZ00iE3Gvmnk',
+    {
+        apiVersion: '2020-08-27',
+    }
+);
 
 /**
  * The client will send the current form values & a payment form field key,
@@ -20,50 +22,67 @@ export async function handler(event: APIGatewayEvent) {
     const pageVersion = event.queryStringParameters?.formVersion ?? null; // used to ensure the correct version of a form is used for billing.
 
     if (!pageId) {
-        return formatError('Bad Request', `Expected missing field 'formId' of type string.`);
+        return formatError(
+            'Bad Request',
+            `Expected missing field 'formId' of type string.`
+        );
     }
     if (!pageVersion) {
-        return formatError('Bad Request', `Expected missing field 'formVersion' of type string.`);
+        return formatError(
+            'Bad Request',
+            `Expected missing field 'formVersion' of type string.`
+        );
     }
 
-    const api = new EventoApi({
-        gatewayUrl: config.api.baseUrl
-    })
-    const result = await api.pages.retrieve(pageId, pageVersion);
-    if (isLeft(result)) {
-        return formatError('Internal Server Error', result.left.message, 500);
-    }
-    const pageConfig = result.right;
+    // const api = new EventoApi({
+    //     gatewayUrl: config.api.baseUrl
+    // })
+    // const result = await api.pages.retrieve(pageId, pageVersion);
+    // if (isLeft(result)) {
+    //     return formatError('Internal Server Error', result.left.message, 500);
+    // }
+    // const pageConfig = result.right;
+    const pageConfig = new PageModel({
+        pages: TestConfig.Pages,
+        calculations: TestConfig.Calculations,
+    });
 
-    let key: string, fields: Record<string, any>;
+    let fields: Record<string, any>, metadata: Record<string, string>;
     try {
         let result: any;
         try {
             result = JSON.parse(event.body);
         } catch (error) {
-            throw new Error('Failed to parse body.')
+            throw new Error('Failed to parse body.');
         }
-        if (!result.key) throw new Error('Missing payment field key.');
         if (!result.fields) throw new Error('Missing form fields.');
 
-        key = result.key;
+        metadata = result.metadata ?? {};
         fields = result.fields;
     } catch (error) {
         return formatError('Bad Request', error.message, 400);
     }
 
-    const amount = pageConfig.getPaymentAmount(key, fields);
+    const amount = pageConfig.getPaymentAmount(fields);
     if (!amount) {
-        return formatError('Bad Request', 'Failed to parse form and retrieve payable amount.', 400)
+        return formatError(
+            'Bad Request',
+            'Failed to parse form and retrieve payable amount.',
+            400
+        );
     }
 
     const paymentIntent = await stripe.paymentIntents.create({
         amount,
-        currency: "aud",
+        currency: 'aud',
         automatic_payment_methods: {
             enabled: true,
         },
+        metadata,
     });
 
-    return formatResponse({clientSecret: paymentIntent.client_secret, amount})(200)
+    return formatResponse({
+        clientSecret: paymentIntent.client_secret,
+        amount,
+    })(200);
 }
